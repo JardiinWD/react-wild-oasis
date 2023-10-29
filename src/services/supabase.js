398de3cef520832f7db116a5e65ee2a8161b13ba
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-10-29 20:01:17 +0100

    Istance of Supabase db, removed images and saved them on supabase bucket

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://atdubmvtwbpcbkkhvlam.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZHVibXZ0d2JwY2Jra2h2bGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1OTgyNjIsImV4cCI6MjAxNDE3NDI2Mn0.VUQRURhgV7hauPE8MsSzG10KL4Evih5psg1oDZCmm-U"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
